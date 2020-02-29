6ccdaf3692654222952ab1adecf6b9530e50d803
iareni22 <ivaneins@hotmail.com>
2020-02-28 22:52:33 -0700

    Completed findTailRecursive along with helper methods, completed test cases

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -61,11 +61,61 @@ public class SnakeGame {
         return findTailRecursive(headPosition, headPosition);
     }
 
-    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
-        if(neighbors(currentPosition[0], currentPosition[1]) == 1){
-            if(previousPosition == headPosition){
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){ //recursiveChecks is being tracked inside of the lengthRec() method along with length
+        if(neighbors(currentPosition[0], currentPosition[1]) == 1 && !isSameCell(currentPosition, previousPosition)){
+            return new int[]{currentPosition[0], currentPosition[1], lengthRec(game)};
+        }
+        int[] futureCell = neighborsRec(currentPosition[0], currentPosition[1], previousPosition[0], previousPosition[1]);
+        previousPosition[0] = currentPosition[0];
+        previousPosition[1] = currentPosition[1];
+        return findTailRecursive(futureCell, previousPosition);
+    }
+
+    private int lengthRec(boolean[][] board){ //keeps track of recursiveChecks as well as length
+        int length = 0;
+        for(int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if(board[i][j]){
+                    length++;
+                    recursiveChecks++;
+                }
             }
         }
+        return length;
+    }
+
+    private boolean isSameCell(int[] currentPosition, int[] previousPosition){
+        return currentPosition[0] == previousPosition[0] && currentPosition[1] == previousPosition[1];
+    }
+
+    private int[] neighborsRec(int row, int col, int prevRow, int prevCol){
+        int[] A = new int[2];
+        if(row - 1 >= 0 && game[row - 1][col]) { //checks top
+            if(!(row - 1 == prevRow && col == prevCol)) {
+                A[0] = row - 1;
+                A[1] = col;
+            }
+        }
+        if(col - 1 >= 0 && game[row][col - 1]) { //checks left
+            if(!(col - 1 == prevCol && row == prevRow)) {
+                A[0] = row;
+                A[1] = col - 1;
+            }
+        }
+        if(col + 1 < game[0].length && game[row][col + 1]) { //checks right
+            if(!(col + 1 == prevCol && row == prevRow)) {
+                A[0] = row;
+                A[1] = col + 1;
+            }
+        }
+        if(row + 1 < game.length && game[row + 1][col]) { //checks bottom
+            if(!(row + 1 == prevRow && col == prevCol)) {
+                A[0] = row + 1;
+                A[1] = col;
+            }
+        }
+        return A;
+
     }
 
     private int neighbors(int row, int col){
--- java/src/SnakeGameTester.java
+++ java/src/SnakeGameTester.java
@@ -121,5 +121,123 @@ public class SnakeGameTester {
 
     //Test cases - findTailRecursive~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    /*
+     * findTailRecursive:
+     * Test case 1
+     * findTailRecursive() → B
+
+     *    Test case ensures proper function of the method, making sure the basic functions work well and all requirements
+     * are met by the test. The test will make sure the tail is found with no problems and the coordinates are placed
+     * correctly.
+     */
+    @Test //Passed
+    public void findTailRecursive1(){
+
+        boolean[][] A = {{false, false, false, true},
+                {false, false, false, true},
+                {false, false, true, true},
+                {false, false, true, false}};
+        int x = 0;
+        int y = 3;
+        int[] B = {3, 2, 5};
+
+        SnakeGame SG = new SnakeGame(A, x, y);
+        assertArrayEquals(B, SG.findTailRecursive());
+    }
+
+    /*
+     * findTailRecursive:
+     * Test case 2
+     * findTailRecursive() → B
+
+     *    Test case accounts for proper tracking of recursiveChecks, making sure the number is well kept and there is no
+     * overflow or shortage of cells counted.
+     */
+    @Test //Passed
+    public void findTailRecursive2(){
+
+        boolean[][] A = {{false, false, true, true},
+                {false, false, false, true},
+                {false, false, true, true},
+                {true, true, true, false}};
+        int x = 0;
+        int y = 2;
+        int[] B = {3, 0, 8};
+        int count = 8;
+
+        SnakeGame SG = new SnakeGame(A, x, y);
+        SG.findTailRecursive();
+        assertEquals(count, SG.getRecursiveChecks());
+    }
+
+    /*
+     * findTailRecursive:
+     * Test case 3
+     * findTailRecursive() → B
+
+     *    Test case accounts for the correct length count while running through the method. Ensures the snake is being
+     * managed properly and no excess or under counting is being done.
+     */
+    @Test //Passed
+    public void findTailRecursive3(){
+
+        boolean[][] A = {{false, false, true, false},
+                {true, false, true, false},
+                {true, true, true, false},
+                {false, false, false, false}};
+        int x = 1;
+        int y = 0;
+        int[] B = {0, 2, 6};
+
+        SnakeGame SG = new SnakeGame(A, x, y);
+        assertArrayEquals(B, SG.findTailRecursive());
+    }
+
+    /*
+     * findTailRecursive:
+     * Test case 4
+     * findTailRecursive() → B
+
+     *    Test case passes instance in which the snake is of a larger length that borders around the board, testing all
+     * directions in which the neighbors could possibly appear.
+     */
+    @Test //Passed
+    public void findTailRecursive4(){
+
+        boolean[][] A = {{true, false, true, true},
+                {true, false, false, true},
+                {true, false, false, true},
+                {true, true, true, true}};
+        int x = 0;
+        int y = 0;
+        int[] B = {0, 2, 11};
+
+        SnakeGame SG = new SnakeGame(A, x, y);
+        assertArrayEquals(B, SG.findTailRecursive());
+    }
+
+    /*
+     * findTailRecursive:
+     * Test case 5
+     * findTailRecursive() → B
+
+     *    Test case checks for the recursiveChecks to solidify the function of the recursiveCheck counter with no issues.
+     */
+    @Test //Passed
+    public void findTailRecursive5(){
+
+        boolean[][] A = {{true, false, true, true},
+                {true, false, false, true},
+                {true, false, false, true},
+                {true, true, true, true}};
+        int x = 0;
+        int y = 0;
+        int[] B = {0, 2, 11};
+        int count = 11;
+
+        SnakeGame SG = new SnakeGame(A, x, y);
+        SG.findTailRecursive();
+        assertEquals(count, SG.getRecursiveChecks());
+    }
 
 }
